9c70639085885ffd5145a4485d6e885e5585fbe7
Tori Kafati <vfcad@protonmail.com>
2025-10-20 22:55:38 -0600

    the rest of the project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import RockyMountainRubyPresentation from "./RockyMountainRubyPresentation";
+
+
+export default function App() {
+  return <RockyMountainRubyPresentation />;
+}
